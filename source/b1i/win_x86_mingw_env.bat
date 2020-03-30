0f0d5acf3ebc76f67c3d5b7eae10877f42783df7
basic-1 <b1justomore@gmail.com>
2020-03-30 17:11:35 +0300

    delete ignored files

--- source/b1i/win_x86_mingw_env.bat
+++
@@ -1,1 +0,0 @@
-set PATH=%PATH%;D:\Program Files\mingw-w64\i686-8.1.0-win32-dwarf-rt_v6-rev0\mingw32\bin
--- source/b1i/win_x86_msvc10_env.bat
+++
@@ -1,3 +0,0 @@
-set PATH=%PATH%;C:\Program Files\Microsoft Visual Studio 10.0\VC\bin
-set LIB=%LIB%;C:\Program Files\Microsoft Visual Studio 10.0\VC\lib
-set INCLUDE=%INCLUDE%;C:\Program Files\Microsoft Visual Studio 10.0\VC\include
