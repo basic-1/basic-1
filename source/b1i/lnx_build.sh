77746e60363d41a7979e999f144a661113f81c52
basic-1 <b1justomore@gmail.com>
2020-04-13 18:49:41 +0300

    build scripts for Linux, initial commit

---
+++ source/b1i/lnx_build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# <project_name> <platform> <compiler> <configuration> <cmake1_options> <is_next_revision>
+
+project_name=$1
+platform=$2
+compiler=$3
+configuration=$4
+
+mkdir ../../build
+mkdir ../../build/lnx
+mkdir ../../build/lnx/$platform
+mkdir ../../build/lnx/$platform/$compiler
+mkdir ../../build/lnx/$platform/$compiler/$configuration
+mkdir ../../build/lnx/$platform/$compiler/$configuration/$project_name
+
+mkdir ../../bin
+mkdir ../../bin/lnx
+mkdir ../../bin/lnx/$platform
+mkdir ../../bin/lnx/$platform/$compiler
+mkdir ../../bin/lnx/$platform/$compiler/$configuration
+
+./get_git_rev.sh "$6"
+
+cd ../../build/lnx/$platform/$compiler/$configuration/$project_name
+
+cmake ../../../../../../source/$project_name $5
+cmake --build . --target $project_name
+
+cp $project_name ../../../../../../bin/lnx/$platform/$compiler/$configuration
+
+cd ../../../../../../source/$project_name
---
+++ source/b1i/lnx_x86_gcc_rel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./lnx_build.sh b1i x86 gcc rel "-DCMAKE_BUILD_TYPE=RELEASE" $1
